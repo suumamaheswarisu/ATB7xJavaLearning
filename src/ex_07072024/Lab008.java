f823decf7753e666cea77c1f54bd90cf43b03267
suumamaheswarisu <su.uma.maheswari.su@gmail.com>
2024-07-25 07:54:04 +0530

    Package renamed

---
+++ src/ex_07072024/Lab008.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class Lab008 {
+    public static void main(String[] Lab008) {
+        System.out.println("Execution starts from Line 4");
+        System.out.println("within double quotes - public static void main(String [] ) - must to add");
+        System.out.println("after string - arg can be anything");
+        System.out.println("In this exercise, the arg is Lab008");
+        System.out.println("To rename the package - Right Click on the package >> Refactor >> Rename");
+    }
+}
